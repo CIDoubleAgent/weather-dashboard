a47f09b63acd04ea55e5f543ea471be5bef3b3cd
Nick Zabkar <nzabkar51@gmail.com>
2021-07-31 00:13:57 -0400

    Added variables and init func to js

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,20 @@
-console.log(window.outerWidth);
+const APIkey = "&appid=f435c899986514624bda61a6f76cde46";
+const weatherAPI = "https://api.openweathermap.org/data/2.5/onecall?";
+const forecastAPI = "https://api.openweathermap.org/data/2.5/forecast?q=";
+const geoAPI = navigator.geolocation;
+const units = "&units=imperial";
+let citySearch;
+let searchHistorArr = [];
+
+$(document).readyState(function() {
+  init();
+
+  function init() {
+    $("#current-forecast").hide();
+    $("#five-day-forecast-container").hide();
+    $("search-history-container").hide();
+    $("error-div").hide();
+  }
+
+  
+});
