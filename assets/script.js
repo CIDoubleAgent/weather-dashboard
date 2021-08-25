bfc656b834c48ac834df3c587ffc7dd304eff5ba
Nick Zabkar <nzabkar51@gmail.com>
2021-08-25 16:36:06 -0400

    Stored json values in variables and displayed to page

--- assets/script.js
+++ assets/script.js
@@ -4,23 +4,50 @@ const onecallAPI = 'https://api.openweathermap.org/data/2.5/onecall?';
 const geocodingAPI = 'http://api.openweathermap.org/geo/1.0/direct?q=';
 
 const searchButton = document.querySelector('#search-button');
-let inputVal = document.querySelector('#input-val');
-let weatherImg = document.querySelector('#weather-image');
+let input = document.querySelector('#input-val');
+let weatherImgEl = document.querySelector('#weather-image');
 let temp = document.querySelector('#temperature');
 let humid = document.querySelector('#humidity');
 let windSpd = document.querySelector('#wind-speed');
-let uv = document.querySelector('#uv-index');
+let uvi = document.querySelector('#uv-index');
 
 searchButton.addEventListener('click', function () {
-  fetch(geocodingAPI + inputVal.value + APIKey)
+  fetch(geocodingAPI + input.value + APIKey)
   .then(response => response.json())
   .then(data => {
     console.log(data[0].lat + ',' + data[0].lon);
+
     let latitude = data[0].lat;
     let longitude = data[0].lon;
+
     fetch(onecallAPI + 'lat=' + latitude + '&lon=' + longitude + units + APIKey)
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+      console.log(data);
+
+      let weatherIconVal = data.current.weather[0].icon;
+      let tempVal = data.current.temp;
+      let humidVal = data.current.humidity;
+      let windSpdVal = data.current.wind_speed;
+      let uviVal = data.current.uvi;
+
+      console.log(weatherIconVal);
+      console.log(tempVal);
+      console.log(humidVal);
+      console.log(windSpdVal);
+      console.log(uviVal);
+
+
+
+      weatherImgEl.innerHTML = weatherIconImg;
+      temp.innerHTML = 'Temperature: ' + tempVal + '° F';
+      humid.innerHTML = 'Humidity: ' + humidVal + ' %';
+      windSpd.innerHTML = 'Wind Speed: ' + windSpdVal + ' MPH';
+      uvi.innerHTML = 'UVI Index: ' + uviVal;
+
+    })
+    .catch(err => console.log('An error has occurred'))
+
   })
 
 });
